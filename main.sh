8ad2337b6e4203c00d1fc92bf83ebabd746652f6
skbtemp12 <73464906+skbtemp12@users.noreply.github.com>
2021-10-15 17:39:13 +0600

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Windows app support
+./wine-setup.sh
+
+# changing boot logo
+./boot-logo.sh
+
+#
+
---
+++ wine-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo dpkg --add-architecture i386;
+
+wget -nc https://dl.winehq.org/wine-builds/winehq.key;
+
+sudo apt-key add winehq.key;
+
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main';
+
+sudo apt update -y;
+
+sudo apt install --install-recommends winehq-stable -y;
+
+dpkg-reconfigure wine-stable-amd64 wine-stable wine-stable-i386
+
