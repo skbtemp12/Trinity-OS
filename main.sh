f137df8a225c2cbba40ccb5258a65518e62ffc02
skbtemp12 <73464906+skbtemp12@users.noreply.github.com>
2021-10-15 19:58:32 +0600

    Add files via upload

--- main.sh
+++ main.sh
@@ -2,13 +2,13 @@
 
 
 # modifying system to get latest packages
-bash scripts/system-mod.sh
+./scripts/system-mod.sh
 
 # changing boot logo
-bash scripts/boot-logo.sh
+./scripts/boot-logo.sh
 
 # Windows app support
-bash scripts/wine-setup.sh
+./scripts/wine-setup.sh
 
 # android support
 #bash scripts/anbox-setup.sh
