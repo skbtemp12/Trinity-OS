676bed95f27b101b60fdfa35b33a307e87da20eb
skbtemp12 <73464906+skbtemp12@users.noreply.github.com>
2021-10-25 01:06:50 +0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,11 +6,14 @@ chmod +x *
 # modifying system to get latest packages
 sudo ./system-mod.sh
 
-# changing boot logo
-sudo ./boot-logo.sh
 
 # Windows app support
 sudo ./wine-setup.sh
+
+
+# changing boot logo
+sudo ./boot-logo.sh
+
 
 # android support
 #sudo ./anbox-setup.sh
