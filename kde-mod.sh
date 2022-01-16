f3d465025e869572ff53ba18a64c1084d2fe7818
skbtemp12 <73464906+skbtemp12@users.noreply.github.com>
2022-01-16 16:34:29 +0600

    Update and rename scripts/debloat-kde.sh to kde-mod.sh

--- kde-mod.sh
+++ kde-mod.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+# installing essentials
+sudo apt install qt5-style-kvantum qt5-style-kvantum-themes gparted -y;
+
+
 # removing apps
 sudo apt remove ksysguard skanlite plasma-discover kwalletmanager ksystemlog usb-creator-kde muon -y;
 
